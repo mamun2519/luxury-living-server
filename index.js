e57c407fdc53c079d28fb6bf31d45108ca302fa0
Md Mamun <juborajvai22@gmail.com>
2023-04-12 15:31:22 +0600

    update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 
 
 
-const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@admin.gtjon.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://mamun:ToYPk4oO3SdmpG3b@admin.gtjon.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
